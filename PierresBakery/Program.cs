a32308f9433a8368abf27b38929ae37018010f0e
tien96ng <tien96ng@gmail.com>
2021-02-26 10:29:59 -0800

    Create Program.cs functionality to prompt user with welcome message, cost placeholder for both bread, pastry, exit, and invalid input.

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -6,7 +6,29 @@ namespace Bakery
   {
     public static void Main() 
     {
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("Here is our Menu:\n - Bread ${BREAD_PRICE_PLACEHOLDER}\n - Pastry ${BREAD_PRICE_PLACEHOLDER}");
+      
+      Prompt("Which would you like to buy [B/P/EXIT]: ");
 
+    }
+
+    public static void Prompt(string msg) {
+      Console.Write(msg);
+      string userSelection = Console.ReadLine().ToUpper();
+      if(userSelection == "EXIT") 
+      {
+        Console.WriteLine("Have a good day!");
+      } 
+      else if(userSelection == "B" || userSelection == "P")
+      {
+        Console.WriteLine("Bread and Pastry PlaceHolder Prompts for Amount to buy.");
+      } 
+      else 
+      {
+        Prompt("INVALID INPUT! Which would you like to buy [B/P/EXIT]: ");
+        // userSelection = Console.ReadLine().ToUpper();
+      }
     }
   }
 }
